9b2faa57a8ae9b085af29563653f636fa801ebac
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2022-12-09 11:47:08 +0800

    12/9

---
+++ Test19.java
@@ -0,0 +1,20 @@
+public class User {
+    public static void main (String [] args) {
+	    int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		int count = 0;
+		int temp;
+		for (int x=0; x< arr.length; x++) {
+		    for (int y=0; y < arr[y+1]) {
+			    if (arr[i] > == 2) {
+				    System.out.println(">>" + i);
+                }
+				count++;
+		}
+		
+		for (int x=0; x<arr.length; x++) {
+		    System.out.println(">>" + iArr[x]);
+		}
+	
+		System.out.println("===>" + x);
+	}
+}
