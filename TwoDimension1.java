33eff9db1601ab3c2181580cff9122f0cd0d4f94
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2022-12-30 11:45:50 +0800

    12/30

---
+++ TwoDimension1.java
@@ -0,0 +1,15 @@
+public class TwoDimension1{
+    public static void main(String[] args) {
+	    int[][] data2 = { {1,2,3,4}, {5,6,7,8} };
+		
+		System.out.println(data2.length); //
+		System.out.println(data2[1].length);
+		
+		
+		for (int i=0; i<3; i++){
+			System.out.println("");
+		}
+	}
+		
+		
+	
