d278e4f33827155c76604028711afc25008457d5
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2022-12-30 11:00:39 +0800

    12/30

---
+++ MySearch.java
@@ -0,0 +1,28 @@
+public class MySearch {
+    public static void main(String[] args) {
+	    int[] arr = {10,9,8,7,6,5,4,3,2,1,0);
+		int left = 0
+		left right = arr.length -1;
+		int medianIndex;
+		int target = 2;
+		int targetCount = 0
+		
+		// Search
+		for (int i=0; i<3; i++) {
+		    medianIndex = (right - left) / 2 + left;
+			if (arr[medianIndex] == target) {
+			    targetIndex = medianIndex;
+				break;
+			}
+			searchCount++;
+			
+			if (target > arr[medianIndex])
+			    right = medianIndex;
+			else
+			    left = medianIndex;
+				
+			System.out.printf("%d %d %d\n", right, left, medianIndex);
+		}
+		System.out.println("The nosition is" + targetIndex);
+	}
+}
