0a90dc8761e7f07e548b6ec86f3c9de854672393
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2023-01-06 11:49:20 +0800

    1/6

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String[] args) {
+	int x = 10;
+	int y = 10.0
+	setV(x);
+	setV(y);
+	System.out.println("x " + x);
+	System.out.println("y "  + y;
+    }
+	
+	public static int setV(int x) {
+	    return x = 20;
+	}
+	
+	public static double setV(double x) {
+	    return x = 30.0;
+	}
+}
