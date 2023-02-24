f04097bd8158d6898201546c4cbfab4c4a5e88d2
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2023-02-24 10:55:58 +0800

    2/24

---
+++ MyArray.java
@@ -0,0 +1,18 @@
+public class MyArray {
+    public static void main(String[] args) {
+		Human.printHeight();
+	}
+}
+ class Human {
+     int sex;
+	 static int height = 180;
+	 private int weighe = 75;
+    
+	public static void printHight() {
+	    System.out.println(height);
+	}
+	
+	public void printWight() {
+	    System.out.println(weight);
+	}
+}
