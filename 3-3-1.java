5880e4939fdac31854be5ebd1098eaa820bf507b
Yu117152 <114204254+Yu117152@users.noreply.github.com>
2023-03-03 11:51:14 +0800

    3/3

---
+++ 3-3-1.java
@@ -0,0 +1,11 @@
+public class Myclass {
+    public static void main(String[] args) {
+	    Human human1;
+		human1 = new Human("man", 180, 80);
+		Human human2;
+		human2 = new Human("man", 130, 30);
+		
+		human.printHight();
+		human2.printWight2;
+	}
+}
---
+++ 3-3.java
@@ -0,0 +1,15 @@
+class Human {
+    String sex;
+	int height;
+	int weight;
+	
+	public void Human(String sex, int height, int weight){
+	    this.sex = sex;
+		this.height = height;
+		this.weight = weight;
+	}
+    
+	public void printHight2(){
+        System.out.println(height);
+	}
+}
